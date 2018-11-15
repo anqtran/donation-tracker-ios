5c696e8a5ebaf610bd71ebe09ed9a1237b0f10bf
anqtran <anqtran@gatech.edu>
2018-11-14 19:16:22 -0500

    init project

---
+++ donation-tracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  donation-tracker
+//
+//  Created by An Tran on 11/14/18.
+//  Copyright © 2018 An Tran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
