8c55453d48302fa8ef42a2f39f48e32025c28c7e
anqtran <anqtran@gatech.edu>
2018-11-24 13:58:13 -0500

    finished Add Item Front End

--- donation-tracker/Controller/AddItemVC.swift
+++ donation-tracker/Controller/AddItemVC.swift
@@ -8,23 +8,56 @@
 
 import UIKit
 
-class AddItemVC: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+class AddItemVC: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource, UITextFieldDelegate {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return categories.count
     }
-    */
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return categories[row]
+    }
 
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        itemCategory = categories[row]
+        categoryTxt.text = itemCategory
+    }
+    @IBOutlet weak var shortDescTxt: UITextField!
+    @IBOutlet weak var valueTxt: UITextField!
+    @IBOutlet weak var categoryTxt: UITextField!
+    @IBOutlet weak var moreInfoTxt: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        createPickerView()
+        dissmissPickerView()
+    }
+    
+    @IBAction func AddItemBtnPressed(_ sender: Any) {
+    }
+    
+    var itemCategory: String?
+    var categories = ["Clothing", "Hat", "Kitchen", "Electronics", " Household", "Other"]
+    
+    func createPickerView() {
+        let pickerView = UIPickerView()
+        pickerView.delegate = self
+        categoryTxt.inputView = pickerView
+    }
+    
+    func dissmissPickerView()
+    {
+        let toolBar = UIToolbar()
+        toolBar.sizeToFit()
+        let doneButton = UIBarButtonItem(title: "Done", style: .plain, target: self, action: #selector(self.dismissKeyBoard))
+        toolBar.setItems([doneButton], animated: false)
+        toolBar.isUserInteractionEnabled = true
+        categoryTxt.inputAccessoryView = toolBar
+    }
+    
+    @objc func dismissKeyBoard() {
+        view.endEditing(true)
+    }
 }
--- donation-tracker/Controller/EmployeeDashboardVC.swift
+++ donation-tracker/Controller/EmployeeDashboardVC.swift
@@ -16,15 +16,12 @@ class EmployeeDashboardVC: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func addItemBtnPressed(_ sender: Any) {
+        performSegue(withIdentifier: FROM_EMPLOYEE_DASHBOARD_TO_ADDITEM, sender: nil)
     }
-    */
-
+    
+    @IBAction func ViewItemBtnPressed(_ sender: Any) {
+        performSegue(withIdentifier: FROM_EMPLOYEE_DASHBOARD_TO_VIEWITEM, sender: nil)
+    }
+    
 }
